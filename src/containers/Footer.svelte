f33cfca93546ab3d6dd273930541939204e7e2d2
CreatiCoding <creaticoding@gmail.com>
2020-03-23 00:49:19 +0900

    creco link add

--- src/containers/Footer.svelte
+++ src/containers/Footer.svelte
@@ -1,13 +1,22 @@
 <script>
-
+  function goCrecoSite() {
+    window.location.href = "//creco.fedev.kr";
+  }
 </script>
 
 <style>
   .footer {
     clear: both;
   }
+  .link {
+    cursor: pointer;
+  }
 </style>
 
-<div class="footer">
-  <div class="">ⓒ Creco developer.</div>
+<div class="footer" on:click={goCrecoSite}>
+  <div class="link">
+    ⓒ
+    <a>Creco</a>
+    developer.
+  </div>
 </div>
